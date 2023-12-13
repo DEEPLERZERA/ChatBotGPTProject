af7d8a1ddda781eace8ac88863d28abd467ad935
Daniel <dani2014games@gmail.com>
2023-12-12 22:21:42 -0300

    [GM-02] feat: Developing new functions to BOT person

--- integracao_api.py
+++ integracao_api.py
@@ -2,22 +2,40 @@ import openai
 import dotenv
 import os
 
+def chat(sua_duvida, nome_personalidade):
+    prompt_sistema = f"""
+            Você se chama {nome_personalidade}, e quero que você incorpore a personalidade sendo ela de alguma forma, responda as perguntas do usuário como se fosse a pessoa
+            especificada. Se a pessoa te chamar por outra coisa que não seja um nome de uma personalidade válida, simplesmente diga que não pode responder a pergunta.
+    """
+    resposta = openai.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {
+                "role": "system",
+                "content": prompt_sistema
+            },
+            {
+                "role": "user",
+                "content": sua_duvida
+            }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        n=5
+    )
+
+    print(resposta.choices[0].message.content)
+
 dotenv.load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-resposta = openai.chat.completions.create(
-    model = "gpt-3.5-turbo",
-    messages = [
-        {
-            "role": "system",
-            "content": "Você é o Aryton Senna, maior piloto da história da Formula 1" 
-        },
-        {
-            "role": "user",
-            "content": "Olá Senna! Sou seu fan qual sua pista favorita?"
-        }
-    ]
 
-)
-
-print(resposta)
+print("Digite sua dúvida:")
+sua_duvida = input()
+while True:
+    print("Digite o nome da personalidade:")
+    nome_personalidade = input()
+    chat(sua_duvida, nome_personalidade)
